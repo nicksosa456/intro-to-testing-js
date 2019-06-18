9b284bfd98121a5f9ed46ed1b365cf81c7cdd166
Nicholas Sosa <nicholassosa@Nicholass-MacBook-Air.local>
2019-06-18 09:22:43 -0500

    finished up to exercise 11

--- code.js
+++ code.js
@@ -10,6 +10,28 @@ function sayHello(input) {
     return "Hello, " + input + "!";
 }
 
+function isFive(a) {
+    if (a===5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isEven(a) {
+    if (a==="boolean") {
+        return false;
+    } else if (a%2===0) {
+        return true;
+    } else if (a%2===1) {
+        return false;
+    } else if (a==="string") {
+        return false;
+    } else {
+        return false;
+    }
+}
+
 // function add(a, b) {
 //     if (typeof a=== "string" || typeof b==="string") {
 //         return false;
--- tests.js
+++ tests.js
@@ -42,6 +42,54 @@ describe('sayHello', function() {
     });
 });
 
+describe('isFive',function() {
+    it('should be a defined function', function() {
+        expect(typeof isFive).toBe('function');
+    });
+    it('should return a boolean when called', function() {
+       expect(typeof isFive()).toBe('boolean');
+    });
+    it('should return true when passed 5', function() {
+       expect(isFive(5)).toBe(true);
+    });
+    it('should return false when passed a string', function() {
+        expect(isFive('5')).toBe(false);
+    });
+});
+
+describe('isEven', function() {
+   it('should be a defined function', function() {
+       expect(typeof isEven).toBe('function');
+   });
+   it('should return a boolean when called', function() {
+       expect(typeof isEven()).toBe('boolean');
+   });
+   it('should return true when passed 2', function() {
+       expect(isEven(2)).toBe(true);
+   });
+   it('should return true when passed -4', function() {
+       expect(isEven(-4)).toBe(true);
+   });
+   it('should return false when passed 3', function() {
+       expect(isEven(3)).toBe(false);
+   });
+   it('should return false when passed banana', function() {
+       expect(isEven("banana")).toBe(false);
+   });
+   it('should return true when passed "8"', function() {
+       expect(isEven("8")).toBe(true);
+   });
+   it('should return false when passed Infinity', function() {
+       expect(isEven(Infinity)).toBe(false);
+   });
+   it('should return false when passed a boolean', function() {
+       expect(isEven(true)).toBe(false);
+   });
+   it('should return false when called without an argument', function() {
+       expect(isEven()).toBe(false);
+   })
+});
+
 // describe('add', function() {
 //    it('should be a defined function', function() {
 //        expect(typeof add).toBe('function');
